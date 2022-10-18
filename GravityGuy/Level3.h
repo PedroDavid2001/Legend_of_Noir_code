2a079fcbb1fbba628768b73862ccb7814faaf526
RicardoJun10r <ricardojunior0110@gmail.com>
2022-10-18 13:44:12 -0300

    Enviando minha fase

---
+++ GravityGuy/Level3.h
@@ -0,0 +1,37 @@
+#ifndef _GRAVITYGUY_LEVEL3_H_
+#define _GRAVITYGUY_LEVEL3_H_
+
+// ------------------------------------------------------------------------------
+// Inclusões
+
+#include "Game.h"
+#include "Scene.h"
+#include "Player.h"
+#include "Font.h"
+#include "Timer.h"
+#include "Snow.h"
+#include "Background.h"
+#include <sstream>
+using std::stringstream;
+
+// ------------------------------------------------------------------------------
+
+class Level3 : public Game
+{
+private:
+    Background* backg = nullptr;   // pano de fundo animado
+    Timer               timer;
+    Font* position =    nullptr;
+    stringstream	    currentPos;
+public:
+    static Scene* scene;           // cena do nível
+
+    void Init();                    // inicialização do nível
+    void Update();                  // atualiza lógica do jogo
+    void Draw();                    // desenha jogo
+    void Finalize();                // finalização do nível
+};
+
+// -----------------------------------------------------------------------------
+
+#endif
